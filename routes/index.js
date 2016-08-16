ecce28656e34a55fb89066ea20e113297776f62d
Emanuela Colta <coltaemanuela@gmail.com>
2016-08-16 13:53:57 +0300

    fixed #1. Sign up/in template added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup',function(req,res,next){
+  res.render('signin', { title: 'Sign up' });
+  }); //tot signin pt cs pastreaza template-ul
+
+router.get('/signin',function(req,res,next){
+  res.render('signin', { title: 'Sign in' });
+  });
 module.exports = router;
